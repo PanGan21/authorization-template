d91ea2df107defc56f75469cde45637261cabc2b
PanGan21 <ganelis.pan@gmail.com>
2021-03-25 21:46:17 +0100

    set user-permission middleware

--- src/server.js
+++ src/server.js
@@ -5,4 +5,13 @@ import { permissions } from "./data.js";
 const app = express();
 app.use(express.json());
 
+const setUser = (req, res, next) => {
+  const userId = req.body.userId;
+  if (userId) {
+    req.user = permissions.find((user) => user.id == userId);
+  }
+
+  next();
+};
+
 app.listen(3000);
