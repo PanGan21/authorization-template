63510a5ebe4576385432e896338e9f35e48d6b04
PanGan21 <ganelis.pan@gmail.com>
2021-03-25 21:39:57 +0100

    add permissions file

---
+++ src/data.js
@@ -0,0 +1,18 @@
+const ROLE = {
+  ADMIN: "admin",
+  BASIC: "basic",
+};
+
+export const permissions = {
+  ROLE,
+  users: [
+    { id: 1, name: "ADMIN-NAME", role: ROLE.ADMIN },
+    { id: 2, name: "BASIC-NAME-1", role: ROLE.BASIC },
+    { id: 3, name: "BASIC-NAME-2", role: ROLE.ADMIN },
+  ],
+  resources: [
+    { id: 1, name: "RESOURCE-1", userId: 1 },
+    { id: 2, name: "RESOURCE-2", userId: 2 },
+    { id: 3, name: "RESOURCE-3", userId: 3 },
+  ],
+};
